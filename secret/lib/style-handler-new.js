41a2c00322e98b09b74d4c8976201d55c644b423
KLPig <kkkkkent@qq.com>
2024-03-10 16:09:14 +0800

    Briefly updates secret

---
+++ secret/lib/style-handler-new.js
@@ -0,0 +1,33 @@
+arr = document.getElementById('arrow')
+menu = document.getElementById('menu')
+
+state = true
+arr.className = 'out'
+
+arr.addEventListener(
+    'click',
+    function(){
+        if(!state){
+            arr.className = 'out'
+            arr.innerHTML = '>'
+            menu.className = 'out'
+            setTimeout(function(){arr.innerHTML = '<'}, 1000)
+        }else{
+            arr.className = 'in'
+            arr.innerHTML = '<'
+            menu.className = 'in'
+            setTimeout(function(){arr.innerHTML = '>'}, 1000)
+        }
+        state = !state;
+    }
+)
+
+localStorage.site = 'secrets/empty.html'
+
+setInterval(
+    function(){
+        scr = document.getElementById('main').childNodes[0]
+        if(scr.src != localStorage.site)
+            scr.src = localStorage.site
+    }, 1000
+)
--- secret/lib/testiflogin.js
+++ secret/lib/testiflogin.js
@@ -1,14 +1,2 @@
 if(localStorage.login == undefined)
     window.open('https://klpig.github.io/secret/lib/notlogin.html', '_self')
-function updatefullscreen(){
-    if(localStorage.fullscreen == 'true'){
-        document.getElementById('main').className = 'fullscreen'
-        document.getElementById('menu').className = 'fullscreen'
-        document.getElementById('title').style.position = 'static'
-    }else{
-        document.getElementById('main').className = undefined
-        document.getElementById('menu').className = undefined
-        document.getElementById('title').style.position = 'sticky'
-    }
-}
-setInterval(updatefullscreen, 10)
