3e0177d8f62ae41b555638d2d320e445923b9639
KLPig <kkkkkent@qq.com>
2024-03-12 18:12:54 +0800

    Change logins

--- secret/lib/method-reader.js
+++ secret/lib/method-reader.js
@@ -1,4 +1,5 @@
 vars = top.location.href.split('?')[1].split('&')
 
-for(var i = 0; i < vars.length; i++)
+for(var i = 0; i < vars.length; i++){
     sessionStorage.setItem('GET' + vars[i].split('=')[0], vars[i].split('=')[1])
+}
--- secret/lib/testiflogin.js
+++ secret/lib/testiflogin.js
@@ -1,4 +1,7 @@
+const date = new Date()
+var nt = Number(date.getFullYear()) * 7 + Number(date.getMonth()) * 11 + Number(date.getDay())
+
 if(localStorage.login == undefined)
     window.open('https://klpig.github.io/secret/lib/notlogin', '_self')
-else if(localStorage.login == 'once')
-    localStorage.login = undefined
+else if(Number(localStorage.login) != nt)
+    window.open('https://klpig.github.io/secret/lib/notlogin', '_self')
